4951213c13cf1bbc2dd8d1ce6edd4feaec9d392c
mykytaso <mykyta.soloviov@gmail.com>
2025-04-09 15:25:41 -0400

    Update S3 storage configuration in settings.py for enhanced security and functionality

--- mykytaso_app/settings.py
+++ mykytaso_app/settings.py
@@ -124,8 +124,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # S3 Bucket for media files
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "mykytaso-s3-bucket"
+AWS_S3_SIGNATURE_NAME = "s3v4"
+AWS_S3_REGION_NAME = "us-east-1"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-
-# AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
-# MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/"
