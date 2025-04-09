c759879ba4cb96a2d42d5c4afd57efe881eb2fb6
mykytaso <mykyta.soloviov@gmail.com>
2025-04-09 16:37:12 -0400

    Update S3 storage configuration in settings.py for improved media file handling

--- mykytaso_app/settings.py
+++ mykytaso_app/settings.py
@@ -120,19 +120,35 @@ STATIC_ROOT = BASE_DIR / "staticfiles" # for production
 # MEDIA_ROOT = BASE_DIR / "media"
 
 
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
+# MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # S3 Bucket for media files
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+#
+# AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME = "mykytaso-s3-bucket"
+# # AWS_S3_SIGNATURE_NAME = "s3v4"
+# AWS_S3_REGION_NAME = "us-east-1"
+# # AWS_S3_FILE_OVERWRITE = False
+# # AWS_DEFAULT_ACL = None
+# # AWS_S3_VERITY = True
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
+AWS_S3_ACCESS_KEY_ID = os.getenv("AWS_S3_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = os.getenv("AWS_S3_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "mykytaso-s3-bucket"
-AWS_S3_SIGNATURE_NAME = "s3v4"
 AWS_S3_REGION_NAME = "us-east-1"
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
