6a8fba8c31c1ed7e6fa5cd767f91abbc3da5934b
mykytaso <mykyta.soloviov@gmail.com>
2025-04-14 16:25:21 -0400

    Add comment creation and deletion routes

--- webapp/urls.py
+++ webapp/urls.py
@@ -15,6 +15,8 @@ from webapp.views import (
     AboutMeView,
     ResumeView,
     MessageCreateView,
+    CommentCreateView,
+    CommentDeleteView,
 )
 
 app_name = "webapp"
@@ -25,7 +27,11 @@ urlpatterns = [
     path("posts/create/", PostCreateView.as_view(), name="post_create"),
     path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
-    path("post/change-position/", PostChangePositionView.as_view(), name="post_change_position"),
+    path(
+        "post/change-position/",
+        PostChangePositionView.as_view(),
+        name="post_change_position",
+    ),
     path(
         "posts/<int:pk>/content/create/",
         BlockCreateView.as_view(),
@@ -43,6 +49,16 @@ urlpatterns = [
     ),
     path("posts/<int:pk>/tag/create/", TagCreateView.as_view(), name="tag_create"),
     path("posts/<int:pk>/tag/delete/", TagDeleteView.as_view(), name="tag_delete"),
+    path(
+        "posts/<int:pk>/comment/create/",
+        CommentCreateView.as_view(),
+        name="comment_create",
+    ),
+    path(
+        "posts/<int:pk>/comment/delete/",
+        CommentDeleteView.as_view(),
+        name="comment_delete",
+    ),
     path("about", AboutMeView.as_view(), name="about_me"),
     path("resume/", ResumeView.as_view(), name="resume"),
     path("send_message/", MessageCreateView.as_view(), name="send_message"),
