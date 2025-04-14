f81b174ce97a3b0d33cd5d4325470da304824e0e
mykytaso <mykyta.soloviov@gmail.com>
2025-04-14 17:24:11 -0400

    Add custom filter to adjust comment timestamps by subtracting hours

--- webapp/templatetags/custom_filters.py
+++ webapp/templatetags/custom_filters.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django import template
 
 register = template.Library()
@@ -6,3 +8,8 @@ register = template.Library()
 @register.filter(name="email_username")
 def email_username(value):
     return value.split("@")[0].capitalize()
+
+
+@register.filter
+def minus_hours(value, hours):
+    return value - timedelta(hours=int(hours))
