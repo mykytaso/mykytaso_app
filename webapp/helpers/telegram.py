fcf324deb9ea373ef608d5387b7e6a0807fdfc70
mykytaso <mykyta.soloviov@gmail.com>
2025-10-04 15:23:33 -0400

    Add Telegram Bot configuration to settings and update message sending logic

--- webapp/helpers/telegram.py
+++ webapp/helpers/telegram.py
@@ -1,16 +1,13 @@
-import os
-
 import requests
-from dotenv import load_dotenv
 
-load_dotenv()
+from mykytaso_app import settings
 
 
 def send_telegram_message(message: str) -> None:
-    url = f"https://api.telegram.org/bot{os.getenv('TELEGRAM_BOT_TOKEN')}/sendMessage"
+    url = f"https://api.telegram.org/bot{getattr(settings, 'TELEGRAM_BOT_TOKEN')}/sendMessage"
 
     payload = {
-        "chat_id": os.getenv("TELEGRAM_CHAT_ID"),
+        "chat_id": getattr(settings, "TELEGRAM_CHAT_ID"),
         "text": message,
         "parse_mode": "HTML",
     }
